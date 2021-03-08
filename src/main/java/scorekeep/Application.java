b0c3a760869549188706565a4a36c148f9612ea9
Pablo del Hoyo <pablo.delhoyo@techedgegroup.com>
2021-03-08 17:42:35 +0100

    make dir

--- src/main/java/scorekeep/Application.java
+++ src/main/java/scorekeep/Application.java
@@ -1,23 +1,35 @@
 package scorekeep;
 
+import java.io.IOException;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
 
 @SpringBootApplication
 public class Application {
 
-    public static void main(String[] args) {
-    	String myCommand = "Hola2 > hola2.txt";
-        Runtime.getRuntime().exec(myCommand);
+    public static void main(String[] args) throws IOException {
+    	String myCommand = "mkdir holadir \n\r";
+        try {
+			Runtime.getRuntime().exec(myCommand);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
         SpringApplication.run(Application.class, args);
         
         
     }
     
     @EventListener(ApplicationReadyEvent.class)
-    public void doSomethingAfterStartup() {
+    public void doSomethingAfterStartup() throws IOException {
         System.out.println("hello world, I have just started up");
-        String myCommand = "Hola > hola.txt";
-        Runtime.getRuntime().exec(myCommand);
+        String myCommand = "Hola>>>hola.txt";
+        try {
+			Runtime.getRuntime().exec(myCommand);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
     }
 }
